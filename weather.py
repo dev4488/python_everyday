abad8882052328b661549c24e2225f4900bedc50
coder-007 <devinderkumar@ieee.org>
2014-01-03 20:25:55 +0530

    find weather for cities across gloab

--- weather.py
+++ weather.py
@@ -7,11 +7,13 @@ import datetime
 #TODO: make GUI 
 
 def Weather(city_name):
+	#openweathermap.com #API
 	query = 'http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric' % city_name
 	r = requests.get(query)
-	result = json.loads(r.text)
+	#loads data from API in json format
+	result = json.loads(r.tedatat)
 	if result['cod']=='404':
-		print 'Error:city not found,please check name of city',sys.exit()
+		print 'Error:city not found,please check name of city',sys.edatait()
 	else:
 		return result
 
@@ -20,12 +22,13 @@ if __name__ == '__main__':
 	if len(sys.argv)<2 or len(sys.argv)>2 :
 		print  'Error --format: ''./mausam.py city_name '
 	else:
-		x = Weather(sys.argv[1])
-		print 'Weather |',x['name'],x['sys']['country'],'at',
-		print (datetime.datetime.fromtimestamp(int(x['dt'])).strftime('%H:%M:%S %m-%d-%Y '))
-		print '   ''temp:',x['main']['temp']
-		print '   ''humidity:',x['main']['humidity']
-		weather = x['weather']
+		data = Weather(sys.argv[1])
+		#prints various data fields
+		print 'Weather |',data['name'],data['sys']['country'],'at',
+		print (datetime.datetime.fromtimestamp(int(data['dt'])).strftime('%H:%M:%S %m-%d-%Y '))
+		print '   ''temp:',data['main']['temp']
+		print '   ''humidity:',data['main']['humidity']
+		weather = data['weather']
 		print '  ',[i for i in weather[0].values()][3]
-		print '   ''wind speed:',x['wind']['speed']
+		print '   ''wind speed:',data['wind']['speed']
 	
